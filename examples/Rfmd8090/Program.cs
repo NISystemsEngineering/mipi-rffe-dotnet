d2411e4897a13323de38965832c1531c74ea5150
Sean Moore <sean.moore@ni.com>
2020-04-24 10:54:01 -0500

    update example with latest polymorphism changes

--- examples/Rfmd8090/Program.cs
+++ examples/Rfmd8090/Program.cs
@@ -2,6 +2,7 @@
 using NationalInstruments.ModularInstruments.NIDigital;
 using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace Rfmd8090
 {
@@ -9,33 +10,48 @@ namespace Rfmd8090
     {
         static void Main(string[] args)
         {
+            // open session and load digital project
             NIDigital session = new NIDigital("PXIe-6570", true, false);
             MipiRffe.LoadDigitalProject(session);
 
+            // create and enable mipi bus
             MipiRffe bus0 = new MipiRffe(session, 0);
             bus0.EnableVIO();
 
-            foreach (var writeParameters in Rfmd8090.Band1Apt)
+            // build command lists
+            List<RffeCommand> writeCommands = new List<RffeCommand>();
+            List<RffeCommand> readCommands = new List<RffeCommand>();
+            foreach (var commandParameters in Rfmd8090.Band1Apt)
             {
-                byte slaveAddress = writeParameters.Item1;
-                ushort registerAddress = writeParameters.Item2;
-                byte[] writeData = writeParameters.Item3;
-                bus0.ExtendedRegisterWrite(slaveAddress, registerAddress, writeData);
+                var (slaveAddress, registerAddress, writeData) = commandParameters;
+
+                RffeCommand writeCommand = new RffeExtendedRegisterWriteCommand(slaveAddress, registerAddress, writeData);
+                writeCommands.Add(writeCommand);
+
+                RffeCommand readCommand = new RffeExtendedRegisterReadCommand(slaveAddress, registerAddress, writeData.Length);
+                readCommands.Add(readCommand);
             }
 
+            // burst commands
+            bus0.Burst(writeCommands);
+            bus0.Burst(readCommands);
+
+            // print results to console
             Console.WriteLine("Slave | Register | Write | Read");
-            foreach(var writeParameters in Rfmd8090.Band1Apt)
+            foreach (var commands in Enumerable.Zip(writeCommands, readCommands, (writeCommand, readCommand) => (writeCommand, readCommand)))
             {
-                var (slaveAddress, registerAddress, writeData) = writeParameters;
-                byte[] readData = bus0.ExtendedRegisterRead(slaveAddress, registerAddress, writeData.Length);
-                string formattedWriteData = '[' + string.Join(",", writeData.Select(val => { return string.Format("0x{0:X2}", val); })) + ']';
-                string formattedReadData = '[' + string.Join(",", readData.Select(val => { return string.Format("0x{0:X2}", val); })) + ']';
-                Console.WriteLine(string.Format("0x{0:X2} | 0x{1:X2} | {2:s} | {3:s}", slaveAddress, registerAddress, formattedWriteData, formattedReadData));
+                var writeCommand = (RffeExtendedRegisterWriteCommand)commands.writeCommand;
+                var readCommand = (RffeExtendedRegisterReadCommand)commands.readCommand;
+                string formattedWriteData = '[' + string.Join(",", writeCommand.RegisterData.Select(val => string.Format("0x{0:X2}", val))) + ']';
+                string formattedReadData = '[' + string.Join(",", readCommand.RegisterData.Select(val => string.Format("0x{0:X2}", val))) + ']';
+                Console.WriteLine(string.Format("0x{0:X2} | 0x{1:X2} | {2:s} | {3:s}", writeCommand.slaveAddress, writeCommand.registerAddress, formattedWriteData, formattedReadData));
             }
 
-            Console.WriteLine("Press any key to exit..");
+            // wait on user
+            Console.WriteLine("Burst complete. Press any key to exit..");
             Console.ReadKey();
 
+            // disable bus and close session
             bus0.DisableVIO();
             session.Close();
         }
